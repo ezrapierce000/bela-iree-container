2e973bd2f2c9e7f14ce2e2fcf79a1930139eb7ac
ezrapierce000 <ezrapierce@cmail.carleton.ca>
2022-07-18 16:48:51 -0400

    Adding IREE build step to Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM debian:bullseye
 ENV DEBIAN_FRONTEND noninteractive
 
-ADD scripts/ ./scripts/
 ADD cmake/ ./cmake/
 ADD iree/ ./iree/
-
+ADD scripts/ ./scripts/
 
 RUN ./scripts/build_packages.sh
 RUN ./scripts/build_bela.sh
 RUN ./scripts/build_env.sh
+RUN ./scripts/build_iree.sh
 
 CMD /bin/bash
--- scripts/build_iree.sh
+++ scripts/build_iree.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+cd /iree
 
+cmake -GNinja -B ../iree-build/ -S . \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DIREE_ENABLE_ASSERTIONS=ON \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
