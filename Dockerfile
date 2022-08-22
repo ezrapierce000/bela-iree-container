b34e8fcad721912a4ad4cfbb8b12d92e7b54b2ce
ezrapierce000 <ezrapierce@cmail.carleton.ca>
2022-08-22 19:07:00 -0400

    updating iree

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,15 @@
 FROM --platform=linux/amd64 debian:bullseye
 ENV DEBIAN_FRONTEND noninteractive
 
-ADD cmake/ /home/cmake/
-ADD iree/ /home/iree/
-ADD models/ /home/models/
-ADD bela-iree-runtime/ /home/bela-iree-runtime/
+ADD cmake /home/cmake
+ADD iree /home/iree
+ADD bela-iree-runtime /home/bela-iree-runtime
 
 ADD scripts/docker-build/build_packages.sh /home/scripts/docker-build/build_packages.sh
 RUN /home/scripts/docker-build/build_packages.sh
 
 ADD scripts/docker-build/build_bela.sh /home/scripts/docker-build/build_bela.sh
 ADD scripts/docker-build/build_settings /home/scripts/docker-build/build_settings
-RUN /home/scripts/docker-build/build_bela.sh
 
 ADD scripts/docker-build/build_env.sh /home/scripts/docker-build/build_env.sh
 RUN /home/scripts/docker-build/build_env.sh
--- scripts/docker-build/build_iree.sh
+++ scripts/docker-build/build_iree.sh
@@ -2,10 +2,10 @@
 
 mkdir /opt/iree-host-build && cd /opt/iree-host-build
 
-wget https://github.com/iree-org/iree/releases/download/candidate-20220717.207/iree-dist-20220717.207-linux-x86_64.tar.xz
+wget https://github.com/iree-org/iree/releases/download/candidate-20220821.242/iree-dist-20220821.242-linux-x86_64.tar.xz
 
-tar xvf iree-dist-20220717.207-linux-x86_64.tar.xz
-rm iree-dist-20220717.207-linux-x86_64.tar.xz
+tar xvf iree-dist-20220821.242-linux-x86_64.tar.xz
+rm iree-dist-20220821.242-linux-x86_64.tar.xz
 
 cd /home/iree/
 
@@ -14,10 +14,10 @@ cmake -B /opt/iree-device-build/ -S . \
         -DIREE_HOST_BINARY_ROOT="/opt/iree-host-build" \
         -DIREE_BUILD_COMPILER=OFF \
         -DIREE_BUILD_TESTS=OFF \
-        -DIREE_BUILD_SAMPLES=ON \
+        -DIREE_BUILD_SAMPLES=OFF \
         -DIREE_DEVICE_SIZE=uint32_t \
         -DIREE_TARGET_BACKEND_DEFAULTS=OFF \
-        -DIREE_TARGET_BACKEND_DYLIB_LLVM_AOT=ON \
+        -DIREE_TARGET_BACKEND_LLVM_CPU=ON \
         -DIREE_TARGET_BACKEND_VMVX=ON \
         -DIREE_HAL_DRIVER_DEFAULTS=OFF \
         -DIREE_HAL_DRIVER_LOCAL_SYNC=ON \
