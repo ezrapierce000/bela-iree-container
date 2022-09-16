cf19170569c84be19cbebb6c298b91e646a127fb
ezrapierce000 <ezrapierce@cmail.carleton.ca>
2022-09-16 12:39:00 -0400

    Adding custom makefiles for libbelafull.

--- scripts/docker-build/build_libs.sh
+++ scripts/docker-build/build_libs.sh
@@ -2,9 +2,12 @@
 
 BBB_ADDRESS=root@192.168.6.2
 
+scp misc/CustomMakefileTop.in $BBB_ADDRESS:/root/Bela/
+scp misc/CustomMakefileBottom.in $BBB_ADDRESS:/root/Bela/
+
 # set date and build libraries
 ssh-keygen -R $1 &> /dev/null || true
 ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 $BBB_ADDRESS "date -s \"`date '+%Y%m%d %T %z'`\" > /dev/null"
-ssh $BBB_ADDRESS "cd Bela && rm lib/*"
+ssh $BBB_ADDRESS "cd /root/Bela && rm lib/*"
 ssh $BBB_ADDRESS "cd Bela && make -f Makefile.libraries cleanall && make -f Makefile.libraries all"
 ssh $BBB_ADDRESS "cd Bela && make lib && make libbelafull"
