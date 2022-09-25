494ae65d06f2f8606a1f1d81e33a6c5118b5aa6e
ezrapierce000 <ezrapierce@cmail.carleton.ca>
2022-09-25 01:37:33 -0400

    more mdrnn updates.

--- models/jax/mdrnn.py
+++ models/jax/mdrnn.py
@@ -4,7 +4,9 @@ from iree.jax import Program
 from jax.nn import relu
 from jax.nn import sigmoid
 from jax.nn import tanh
-from jax import numpy as jnp, random, lax, jit
+from jax import numpy as jnp
+from jax import random as jrandom, lax, jit
+from numpy import random as nrandom
 from flax import linen as nn
 from collections import namedtuple
 
@@ -22,25 +24,47 @@ out_dim=2
 
 Params = namedtuple("params", "x,c,h")
 
-
-x = jnp.ones(2)
+x = jnp.ones(out_dim)
 cell_state = jnp.ones((1, hidden_units))
 hidden_state = jnp.ones((1, hidden_units))
 
 params = Params(x, cell_state, hidden_state)
 
-class MDRNN(nn.Module):
+@jit
+def softmax(w, t=1.0):
+  e = jnp.array(w) / t
+  e -= e.max()
+  e = jnp.exp(e)
+  dist = e / jnp.sum(e)
+  return dist
 
+
+def sample_from_output(mus, sigmas, pis):
+  pis = softmax(pis)
+  m = 5 # nrandom.choice(range(len(pis)), p=pis)
+  mus_vector = mus[m * out_dim:(m + 1) * out_dim]
+  sig_vector = sigmas# [m * out_dim:(m + 1) * out_dim]
+  scale_matrix = jnp.identity(out_dim) * sig_vector
+  cov_matrix = np.matmul(scale_matrix, scale_matrix.T)
+  cov_matrix = cov_matrix * sigma_temp
+  sample = nrandom.multivariate_normal(mus_vector, cov_matrix, 1)
+  return sample
+
+
+class MDRNN(nn.Module):
     @nn.compact
     def __call__(self, carry, inputs):
-        carry, inputs = nn.OptimizedLSTMCell()(carry,inputs)
-        carry, inputs = nn.OptimizedLSTMCell()(carry,inputs)
-        carry, inputs = nn.OptimizedLSTMCell()(carry,inputs)
+      # LSTM layer 1
+      carry, inputs = nn.OptimizedLSTMCell()(carry,inputs)
+      # LSTM layer 2
+      carry, outputs = nn.OptimizedLSTMCell()(carry,inputs)
+      # Three Dense layers then predict weights, centres & scales
+      mdn_mus = nn.Dense(num_mixtures*out_dim)(outputs)
+      mdn_sigmas = nn.elu(nn.Dense(num_mixtures*out_dim)(outputs))
+      mdn_pis = nn.Dense(num_mixtures)(outputs)
+      return sample_from_output(mdn_mus, mdn_sigmas, mdn_pis)
 
-        # Three Dense layers then predict weights, centres & scales
-
-        return nn.OptimizedLSTMCell()(carry,inputs)
-
+    
     # defines shape of input?
 
 mdrnn = MDRNN()
@@ -62,13 +86,13 @@ class Empi(Program):
 
 
   def run(self, x=Program.like(x)):
-    (self._cell_state, self._hidden_state), result = self._lstm((self._cell_state,self._hidden_state), x)
-    return result
+    mus, sigmas, pis = self._lstm((self._cell_state,self._hidden_state), x)
+    return mus, sigmas, pis
 
   @Program.kernel
   def _lstm(carry, inp):
-    carry, out = mdrnn.apply({'params': params}, carry, inp)
-    return carry, out
+    mus, sigmas, pis = mdrnn.apply({'params': params}, carry, inp)
+    return mus, sigmas, pis
 
 
 model = Empi()
